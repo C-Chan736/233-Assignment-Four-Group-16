afd6bc9c5973fca4f68962c2ef28c798be43724b
Hunter Blakeman <hunterblakeman77@gmail.com>
2026-04-30 11:43:33 +1200

    Updates to method 4 and creation of a test function

--- test_markovchain.py
+++ test_markovchain.py
@@ -1,2 +1,25 @@
 #This is to test our file
+import numpy as np
 from module_markovchain import MarkovChain
+
+
+
+def test_method_4():
+    chain = MarkovChain()
+
+    chain.state = np.array([235.0, 365.0])
+    chain.transition_matrix = np.array([
+        [0.7, 0.2],
+        [0.3, 0.8]
+    ])
+    chain.required_steps = 3
+
+    chain.write_solution_to_file("test_output.txt")
+
+    with open("test_output.txt", "r") as fp:
+        print(fp.read())
+
+
+test_method_4()
+
+
