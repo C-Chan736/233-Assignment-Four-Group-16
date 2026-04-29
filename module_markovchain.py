85458066fef277a7f3d73d3e3513fc1cea6d623b
C-Chan736 <163789980+C-Chan736@users.noreply.github.com>
2026-04-30 10:13:45 +1200

    Add files via upload

---
+++ module_markovchain.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class MarkovChain:
+    def __init__(self):
+        self.time_step = 0
+        self.state = None
+        self.transition_matrix = None
+        self.required_steps = None
+
+    def read_chain_from_file(self,path):
+        pass
+
+    def step(self):
+        pass
+
+    def check_regularity(self):
+        pass
+
+    def write_solution_to_file(self,path):
+        pass
