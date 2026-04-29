131b47832191d0693a4c398436e2ce16a1f814d3
Claudia Chan <claudia21chan@gmail.com>
2026-04-30 10:57:13 +1200

    test comment for hunter

--- module_markovchain.py
+++ module_markovchain.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class MarkovChain:
     def __init__(self):
         self.time_step = 0
@@ -7,16 +8,15 @@ class MarkovChain:
         self.transition_matrix = None
         self.required_steps = None
 
-    def read_chain_from_file(self,path):
+    # test again lol
+    def read_chain_from_file(self, path):
         pass
 
     def step(self):
         pass
 
     def check_regularity(self):
-
-        
         pass
 
-    def write_solution_to_file(self,path):
+    def write_solution_to_file(self, path):
         pass
